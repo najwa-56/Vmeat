9fe289c3cd43ef5b38ef9d731d5df00e22b75891
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2022-03-11 16:09:34 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,10 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vmeat
-        </a>
+        <div>
+
+         <h1>Vmeat</h1>
+        </div>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
                  <ul>
@@ -21,8 +17,6 @@ class Navbar extends Component {
 
           </li>
         </ul>
-
-
       </nav>
     );
   }
