134c41c2ade033c4e6f7613303e13b2168bca0e5
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2022-03-11 16:08:19 +0300

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,13 @@ class Main extends Component {
           event.preventDefault()
           const name = this.productName.value
           const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
-          this.props.createProduct(name, price)
+          const lastvaccain = this.lastvaccain.value
+          const farmname = this.farmname.value
+          const meatshopname = this.meatshopname.value
+          const farmlocation = this.farmlocation.value
+          const phone = this.phone.value
+
+          this.props.createProduct(name, price,lastvaccain,farmname,meatshopname,farmlocation,phone)
         }}>
           <div className="form-group mr-sm-2">
             <input
@@ -30,6 +36,52 @@ class Main extends Component {
               placeholder="Product Price"
               required />
           </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="lastvaccain"
+              type="text"
+              ref={(input) => { this.lastvaccain = input }}
+              className="form-control"
+              placeholder="date Of lastVac"
+              required />
+          </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="farmname"
+              type="text"
+              ref={(input) => { this.farmname = input }}
+              className="form-control"
+              placeholder="farm name"
+              required />
+          </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="meatshopname"
+              type="text"
+              ref={(input) => { this.meatshopname = input }}
+              className="form-control"
+              placeholder="meatshop name"
+              required />
+          </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="farmlocation"
+              type="text"
+              ref={(input) => { this.farmlocation = input }}
+              className="form-control"
+              placeholder="farm location"
+              required />
+          </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="phone"
+              type="text"
+              ref={(input) => { this.phone = input }}
+              className="form-control"
+              placeholder="phone"
+              required />
+          </div>
+
           <button type="submit" className="btn btn-primary">Add Product</button>
         </form>
         <p>&nbsp;</p>
@@ -40,6 +92,12 @@ class Main extends Component {
               <th scope="col">#</th>
               <th scope="col">Name</th>
               <th scope="col">Price</th>
+              <th scope="col">date Of lastVac</th>
+              <th scope="col">farm name</th>
+              <th scope="col">meatshop name</th>
+              <th scope="col">farm location</th>
+              <th scope="col">Farmer phone</th>
+
               <th scope="col">Owner</th>
               <th scope="col"></th>
             </tr>
@@ -51,6 +109,11 @@ class Main extends Component {
                   <th scope="row">{product.id.toString()}</th>
                   <td>{product.name}</td>
                   <td>{window.web3.utils.fromWei(product.price.toString(), 'Ether')} Eth</td>
+                  <td>{product.lastvaccain}</td>
+                  <td>{product.farmname}</td>
+                  <td>{product.meatshopname}</td>
+                  <td>{product.farmlocation}</td>
+                  <td>{product.phon}</td>
                   <td>{product.owner}</td>
                   <td>
                     { !product.purchased
