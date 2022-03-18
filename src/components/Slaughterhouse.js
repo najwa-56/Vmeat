8feba179f98806354b015a8a10a7ddf243a6cd73
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2022-03-19 00:08:10 +0300

    Update Slaughterhouse.js

--- src/components/Slaughterhouse.js
+++ src/components/Slaughterhouse.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import Marketplace from '../abis/Marketplace.json'
 import Web3 from 'web3'
+import logo from '../logo.png';
+import './App.css';
+import Contract2 from '../abis/Contract2.json'
+import Navbar from './Navbar'
 import SlaughterhouseMain from './SlaughterhouseMain'
+import Farmerp from './Farmerp';
+import {BrowserRouter as Router , Routes } from "react-router-dom";
 
+import Main from './Main'
 
 
 class Slaughterhouse extends Component {
@@ -31,21 +37,26 @@ class Slaughterhouse extends Component {
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
     const networkId = await web3.eth.net.getId()
-    const networkData = Marketplace.networks[networkId]
-    if(networkData) {
-      const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
-      this.setState({ marketplace })
-      const productCount = await marketplace.methods.productCount().call()
+    const networkData= Contract2.networks[networkId]
+    if(networkData ) {
+      const marketplace1 = web3.eth.Contract(Contract2.abi, networkData.address)
+      this.setState({ marketplace1 })
+      const productCount = await marketplace1.methods.productCount().call()
       this.setState({ productCount })
       // Load products
       for (var i = 1; i <= productCount; i++) {
-        const product = await marketplace.methods.products(i).call()
-        this.setState({  products: [...this.state.products, product] })
+        const product1 = await marketplace1.methods.products2(i).call()
+        this.setState({
+          products2: [...this.state.products2, product1]
+        
+        })
+       
       }
       this.setState({ loading: false})
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
     }
+  
   }
 
   constructor(props) {
@@ -53,33 +64,51 @@ class Slaughterhouse extends Component {
     this.state = {
       account: '',
       productCount: 0,
-      products: [],
+      
+      products2: [],
+      
+     
       loading: true
-    }}
+    }
+    this.createProduct2=this.createProduct2.bind(this)
+   
+  }
+  createProduct2(AccessTime,Timetogoout){
+    this.setState({ loading: true })
+    this.state.marketplace1.methods.createProduct2(AccessTime,Timetogoout).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+
+  }
 
   render() {
     return (
-      
-      
       <div>
-        
-
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex">
+             
               { this.state.loading
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                : <SlaughterhouseMain
-                  products={this.state.products}
-                  account={this.state.account}
-                    />
+                : <SlaughterhouseMain 
+                
+                  products2={this.state.products2}
+                 
+                  createProduct2={this.createProduct2}
+
+                  /> 
+               
               }
             </main>
+            
           </div>
         </div>
       </div>
     );
   }
+  
 }
 
 export default Slaughterhouse;
